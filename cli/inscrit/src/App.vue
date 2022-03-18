d91d8fb5cd6902e0ea18684d35d7b53b32419d79
Ndaboom <ndabosam084@gmail.com>
2022-03-18 11:12:47 +0100

    Inscrits app

---
+++ cli/inscrit/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Liste des inscrits</h1>
+  <Inscrits_vue :liste="personnes"></Inscrits_vue>
+</template>
+
+<script>
+import Inscrits_vue from './components/Inscrits.vue';
+export default {
+    name: 'App',
+    components:{
+        Inscrits_vue
+    },
+    data(){
+        return {
+             personnes: [
+                { prenom: "Jean", nom: "Bréhat", age: 20 },
+                { prenom: "Léa", nom: "Prijean", age: 25 },
+                { prenom: "Chis", nom: "Martin", age: 38 },
+                { prenom: "Pierre", nom: "Petit", age: 18 },
+            ],
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ cli/inscrit/src/components/Inscrits.vue
@@ -0,0 +1,33 @@
+<template>
+  <table border>
+      <tr>
+          <th>Prénom</th>
+          <th>Nom</th>
+          <th>Âge</th>
+      </tr>    
+      <tr v-for="(personne,index) in liste" :key="index">
+          <td>{{personne.prenom}}</td>
+          <td>{{personne.nom}}</td>
+          <td>{{personne.age}}</td>
+      </tr>    
+  </table>     
+</template>
+
+<script>
+export default {
+    name: 'Inscrits_vue',
+    props: ['liste']
+}
+</script>
+
+<style>
+  th {
+      background: lightgrey;
+      font-size: 140%;
+  }
+  td {
+      width: 10rem;
+      text-align: center;
+  }
+
+</style>
