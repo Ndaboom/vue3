8bc73676c75353406e2d1f83a9242e17bda1f870
Ndaboom <ndabosam084@gmail.com>
2022-03-18 11:51:45 +0100

    Inscrit2 app with axios done

---
+++ cli/inscrit2/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Liste des inscrits</h1>
+  <Users_list></Users_list>
+</template>
+
+<script>
+import Users_list from './components/Users.vue'
+export default {
+  name: 'App',
+  components: {
+    Users_list
+  }
+
+}
+</script>
+<style>
+  h1 {
+    text-align: center;
+    margin-bottom: 4rem;
+  }  
+</style>
---
+++ cli/inscrit2/src/components/Users.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Nom</th>
+        <th>Mail</th>
+        <th>Ville</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(un, index) in donnees" :key="index">
+          <td>{{index+1}}</td>
+          <td>{{un.name.first}} {{un.name.last}}</td>
+          <td>{{un.email}}</td>
+          <td>{{un.location.city}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "Users_list",
+  data() {
+    return {
+      donnees: [],
+    };
+  },
+  created() {
+    this.axios
+      .get("https://randomuser.me/api/?results=10")
+      .then((utilisateurs) => {
+        this.donnees = utilisateurs.data.results;
+      });
+  },
+};
+</script>
+
+<style>
+</style>
