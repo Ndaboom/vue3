f2c82a08494ed651d460d1438d4691523a90fe38
Ndaboom <ndabosam084@gmail.com>
2022-03-18 16:23:36 +0100

    Composition api

---
+++ cli/composition/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+ <majuscules_cpm></majuscules_cpm>
+</template>
+
+<script>
+import majuscules_cpm from './components/Majuscules-ref.vue';
+
+export default {
+  name: 'App',
+  components: {
+    majuscules_cpm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ cli/composition/src/components/Majuscules-ref.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="maj">
+      <h2>Composition API</h2>
+      <input type="text" v-model="entree" @keyup="enMaj">
+      <span>{{sortie}}</span>
+  </div>    
+</template>
+
+<script>
+import {ref} from 'vue';
+export default {
+    name: 'majuscules_cpm',
+    setup() {
+        let entree = ref('');
+        let sortie = ref('');
+        function enMaj() {
+            sortie.value = entree.value.toUpperCase();
+        }
+        return {
+            entree,
+            sortie,
+            enMaj
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
